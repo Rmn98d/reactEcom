af026607d07a5681e18547f391c8a659ee173981
Rmn <108113450+Rmn98d@users.noreply.github.com>
2024-06-28 14:35:50 +0530

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,28 +3,36 @@ import { useState } from 'react';
 import Footer from './Footer'
 import './nav.css'
 
+import axios from 'axios';
+
 function ContactForm() {
 
-  const [formData, setFormData]=useState({
-    name:'',email:'',detail:'',
-  })
+
+  const [name,setName]=useState('');
+  const [email,setEmail]=useState('');
+  const [details,setDetails]=useState('');
 
 
 
-  const handleData=(e)=>{
-    const{name,value}=e.target;
+const handleSubmit= async(e)=>{
+  e.preventDefault();
+  try{
+    const response = await axios.post('http://127.0.0.1:8000/api/inputData',{name,email,details});
 
-    setFormData((preventDefault)=>({
-     ...preventDefault,
-     [name]:value,
-    }));
+
+    alert('form successfully submit');
+
+    console.log(`sent successfully`,response.data);
+    setName('');
+    setEmail('');
+    setDetails('');
   }
-
-
-const onSubmit= ( e )=>{
-e.preventDefault();
-console.log('formData',formData);
+    catch(error){
+     console.error('error sending data:',error);
+  }
+  
 }
+
 
 
 
@@ -57,29 +65,31 @@ const onViaCallSubmit = ( ) => {
            </button>
            
         <div className='col-5'>
-        <form  onSubmit={onSubmit}>
+        <form  onSubmit={handleSubmit}>
             <div className='formContainer'>
 
 
-             <label htmlFor='name'>Name :</label>
-             <input type='text' name='name' value={formData.name} onChange={handleData} required />
+           
+             <input type='text' name='name' value={name} onChange={(e)=>setName(e.target.value)} required />
 
              <label htmlFor='email'>Email :</label>
-             <input type='email' name='email' value={formData.email}  onChange={handleData} required minlength="6" maxlength="6"/>
+             <input type='email' name='email' value={email}  onChange={(e)=>setEmail(e.target.value)} required />
 
              <label htmlFor='detail'>Detail :</label>
-             <textarea style={{ height: '7em' }} type='text'name='detail' value={formData.detail} onChange={handleData} required></textarea>
+             <textarea style={{ height: '7em' }} type='text'name='detail' value={details} onChange={(e)=>setDetails(e.target.value)} required></textarea>
 
             </div>
+            <div className='submit'>
+             <button type='submit' onClick={handleSubmit}>Submit</button>
+           </div>
 
            </form>
 
         </div>
            
-           <div className='submit'>
-             <button type='submit' onClick={onSubmit}>Submit</button>
-           </div>
+         
 
+           
 
           </div>
         
@@ -91,4 +101,4 @@ const onViaCallSubmit = ( ) => {
   )
 }
 
-export default ContactForm
+export default ContactForm;
